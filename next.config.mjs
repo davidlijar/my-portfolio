7ca9e4590befd3b74ce32e6c84ecd8ebbdec694b
Li Jar <davidbeljar@gmail.com>
2024-06-26 17:02:34 +0900

    adding more icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+//const nextConfig = {};
 
-export default nextConfig;
+//export default nextConfig;
+
+// next.config.js
+const nextConfig = {
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
