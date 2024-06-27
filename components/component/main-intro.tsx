a5a66efcf9c7f02d94d72c44047474671fdd2e1c
davidlijar <davidbeljar@gmail.com>
2024-06-28 07:38:54 +0900

    edited main-intro

--- components/component/main-intro.tsx
+++ components/component/main-intro.tsx
@@ -1,26 +1,27 @@
-import Image from "next/image";
-import { WriterEffect } from "./typewriter-effect";
+import Image from 'next/image'
+import { WriterEffect } from './typewriter-effect'
 
 const MainIntro = () => {
   const message1 = [
-    "I am a software developer who loves to solve problems with coding and thinking. Curiosity makes me fall in love with programming and thinking.",
-  ];
+    'LI JAR',
+    'Software Developer',
+    'Computer Engineering Student',
+  ]
 
   const message2 = [
-    "I believe that I can contribute my skils in coding and computer engineering could makes impacts on chaing of the worlds.",
-  ];
+    'Welcome to my portfolio! I am LI JAR, a software developer and a dedicated computer engineering student. With extensive experience in web development, I have honed my skills in creating dynamic and responsive web applications. My journey has also led me to explore and contribute to projects in IoT, AI, and E-commerce, showcasing my versatility and adaptability in various technological domains. Beyond coding, I excel in leadership and teamwork, consistently driving projects to success through collaboration and innovation.',
+  ]
 
   return (
     <section className="container mx-auto px-4 md:px-6 lg:px-8" id="about">
       <div className="grid gap-8 lg:grid-cols-2 lg:gap-16">
         <div className="flex flex-col items-start gap-4">
           <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
-            LI JAR
+            <WriterEffect str={message1} />
           </h2>
           <div className="text-gray-500 dark:text-gray-400">
-            <WriterEffect str={message1} />
+            <p>{message2}</p>
           </div>
-          
 
           {/* <Button className="mt-4" variant="outline">
                 Download Resume
@@ -33,15 +34,15 @@ const MainIntro = () => {
             height={400}
             src="/lijar.jpg"
             style={{
-              aspectRatio: "400/400",
-              objectFit: "cover",
+              aspectRatio: '400/400',
+              objectFit: 'cover',
             }}
             width={400}
           />
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default MainIntro;
+export default MainIntro
